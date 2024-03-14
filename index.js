4cb45329e213d78c6fb31751fa4129e78eabfab0
Justin <garciajustinkyle229@gmail.com>
2024-03-14 15:44:57 +0800

    Pogi

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 console.log("Hello World.");
 
-function generateRandomNumber(){
-    let randomNum = Math.floor(Math.random() * 10) + 4;
-      
-    if(randomNum >= 5){
-        console.log(`Random Number is > = to 15`);
-     }else{
-        console.log(`Random Number is < = to 15`);
-     }
+generateRandomNumber(30);
 
-    return console.log(randomNum);
+function generateRandomNumber(max){
+    let randomNum = Math.floor(Math.random() * max) + 1;
+      
+    if(randomNum <= 15) {
+        return console.log(`Random Number  ${randomNum} is in B. 1 - 15`)
+     } else if (randomNum >=16 && randomNum <=30){
+        return console.log(`Random Number ${randomNum} is in I. 16 - 30`)
+    } else if (randomNum >=16 && randomNum <=45){
+        return console.log(`Random Number ${randomNum} is in N. 31 - 45`)
+    } else if (randomNum >=16 && randomNum <=60){
+        return console.log(`Random Number ${randomNum} is in G. 46 - 60`)
+    } else if (randomNum >=16 && randomNum <=75){
+        return console.log(`Random Number ${randomNum} is in O. 61 - 75`)
+     }else{
+     
+    return console.log(`Random number is invalid.`)
+     }
 }
